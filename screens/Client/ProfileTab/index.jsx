54375ca794680643437a61f3e8239fc1b7ec31fa
Gregorio Bujor Santos <goyobujor@gmail.com>
2023-05-14 16:02:22 +0200

    feature: Add signout command

--- screens/Client/ProfileTab/index.jsx
+++ screens/Client/ProfileTab/index.jsx
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Pressable, StyleSheet, Text, Touchable, View} from 'react-native'
+import React, {useCallback} from 'react'
+import { getAuth, signOut } from "firebase/auth";
+import * as SecureStore from "expo-secure-store";
 
-const ProfileScreen = () => {
+const ProfileScreen = ({navigation}) => {
+  const auth = getAuth();
+
+  const handleSignOut = useCallback(() => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        SecureStore.deleteItemAsync("user");
+        SecureStore.deleteItemAsync("token");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  }, []);
+
   return (
     <View>
       <Text>ProfileScreen</Text>
+        <Pressable onPress={handleSignOut}>
+            <Text>Logout</Text>
+        </Pressable>
+
     </View>
   )
 }
