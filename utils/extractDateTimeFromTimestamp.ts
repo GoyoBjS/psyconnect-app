d36e5d40039527b73b7fb19ff43a5290c35b571a
Gregorio Bujor Santos <goyobujor@gmail.com>
2023-05-15 14:51:00 +0200

    fix: timestamp now its in seconds

--- utils/extractDateTimeFromTimestamp.ts
+++ utils/extractDateTimeFromTimestamp.ts
@@ -1,7 +1,7 @@
 const SPANISH_TIMEZONE = 2 *3600;
 
 export const extractDateTimeFromTimestamp = (timestamp: number) => {
-    const date =  new Date((timestamp + SPANISH_TIMEZONE)*1000);
+    const date =  new Date((timestamp + SPANISH_TIMEZONE));
     return date.getUTCFullYear() +
         '-' + ('0' + date.getUTCMonth()).slice(-2) +
         '-' + ('0' + date.getUTCDate()).slice(-2) +
@@ -10,14 +10,14 @@ export const extractDateTimeFromTimestamp = (timestamp: number) => {
 }
 
 export const extractDateFromTimestamp = (timestamp: number) => {
-    const date =  new Date((timestamp + SPANISH_TIMEZONE)*1000);
+    const date =  new Date((timestamp + SPANISH_TIMEZONE));
     return date.getUTCFullYear() +
         '-' + ('0' + date.getUTCMonth()).slice(-2) +
         '-' + ('0' + date.getUTCDate()).slice(-2);
 }
 
 export const extractTimeFromTimestamp = (timestamp: number) => {
-    const date =  new Date((timestamp + SPANISH_TIMEZONE)*1000);
+    const date =  new Date((timestamp + SPANISH_TIMEZONE));
     return ('0' + date.getUTCHours()).slice(-2) +
         ':' + ('0' + date.getUTCMinutes()).slice(-2)
 }
