c49069e5bd58a34e671a8f3b334962dca4a1654f
Gregorio Bujor Santos <goyobujor@gmail.com>
2023-05-23 19:12:35 +0200

    feature: add reason pics

--- src/components/reasons.ts
+++ src/components/reasons.ts
@@ -7,65 +7,67 @@ export interface Reasons {
   image: ImageRequireSource
 }
 
+const REASONS_URL = '../assets/reasons/'
+
 export const reasons: Array<Reasons> = [
   {
     id: 1,
     name: 'No lo sé',
     color: '#FFD700',
-    image: require('../assets/feelings/feliz.png')
+    image: require(`${REASONS_URL}nolose.png`)
   },
   {
     id: 2,
     name: 'Amigos',
     color: '#1E90FF',
-    image: require('../assets/feelings/triste.png')
+    image: require(`${REASONS_URL}amigos.png`)
   },
   {
     id: 3,
     name: 'Familiares',
     color: '#F4C534',
-    image: require('../assets/feelings/enfadado.png')
+    image: require(`${REASONS_URL}familia.png`)
   },
   {
     id: 4,
     name: 'Trabajo',
     color: '#667EFF',
-    image: require('../assets/feelings/ansioso.png')
+    image: require(`${REASONS_URL}trabajar.png`)
   },
   {
     id: 5,
     name: 'Pensamientos',
     color: '#FF00FF',
-    image: require('../assets/feelings/estresado.png')
+    image: require(`${REASONS_URL}pensar.png`)
   },
   {
     id: 6,
     name: 'Mascotas',
     color: '#808080',
-    image: require('../assets/feelings/aburrido.png')
+    image: require(`${REASONS_URL}mascotas.png`)
   },
   {
     id: 7,
     name: 'Situación externa',
     color: '#CCBB34',
-    image: require('../assets/feelings/cansado.png')
+    image: require(`${REASONS_URL}problem.png`)
   },
   {
     id: 8,
     name: 'Estudios',
     color: '#FFFF00',
-    image: require('../assets/feelings/nervioso.png')
+    image: require(`${REASONS_URL}estudios.png`)
   },
   {
     id: 9,
     name: 'Pareja',
     color: '#00FFFF',
-    image: require('../assets/feelings/celoso.png')
+    image: require(`${REASONS_URL}pareja.png`)
   },
   {
     id: 10,
     name: 'Deporte',
     color: '#FFA07A',
-    image: require('../assets/feelings/emocionado.png')
+    image: require(`${REASONS_URL}deporte.png`)
   }
 ]
