dcaffc4136d873310315f00249a4583837bec70d
Gregorio Bujor Santos <goyobujor@gmail.com>
2023-05-28 15:01:08 +0200

    feat: add SolutionScreen

---
+++ src/screens/Client/HomeTab/Registrations/Steps/SolutionScreen.tsx
@@ -0,0 +1,141 @@
+import { Dimensions, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { feelings } from '../../../../../components/feelings'
+import ItemCard from '../../../../../components/ItemCard'
+import HeaderRegister from '../../../../../components/HeaderRegister'
+import { QuickRegistrationDataType } from '../../../../../types/quickRegistrationData.type'
+
+const { width } = Dimensions.get('window')
+
+interface Props {
+  setData: (data: any) => void
+  step?: number
+  setStep?: (step: number) => void
+  handleClose?: () => void
+}
+const SolutionScreen = ({ setData, step, setStep, handleClose }: Props) => {
+  const [stopFeeling, setStopFeeling] = useState(false)
+
+  const renderItem = ({ item }: any) => (
+    <ItemCard
+      item={item}
+      handlePress={() => {
+        console.log('item', item.name)
+        setData((prevData: QuickRegistrationDataType) => ({
+          ...prevData,
+          solution: item.name
+        }))
+        if (handleClose) handleClose()
+        if (step && setStep) setStep(step + 1)
+      }}
+    />
+  )
+
+  const headerComponent = (title: string) => <HeaderRegister title={title} />
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>¿Sigues sintiéndote así?</Text>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setData((prevData: QuickRegistrationDataType) => ({
+                ...prevData,
+                solution: 'No'
+              }))
+              if (handleClose) handleClose()
+              if (step && setStep) setStep(step + 1)
+              setStopFeeling(false)
+            }}
+          >
+            <Text style={styles.buttonText}>Si</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setStopFeeling(true)
+            }}
+          >
+            <Text style={styles.buttonText}>No</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {stopFeeling && (
+        <FlatList
+          data={feelings}
+          renderItem={renderItem}
+          numColumns={2}
+          ListHeaderComponent={() => headerComponent('¿Cómo te sientes?')}
+          // StickyHeaderComponent={() =>
+          //   headerComponent({ title: "¿Cómo te sientes?" })
+          // }
+          stickyHeaderIndices={[0]}
+          stickyHeaderHiddenOnScroll={false}
+        />
+      )}
+      {!stopFeeling && (
+        <View style={styles.subcontainer}>
+          <Text style={styles.title}>Esperemos que esté todo bien</Text>
+          <Image source={require('../../../../../assets/thinking.png')} style={styles.image} />
+        </View>
+      )}
+    </View>
+  )
+}
+
+export default SolutionScreen
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 36,
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+    color: '#3253FF'
+  },
+  buttonsContainer: {
+    width: width,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+    gap: 12
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3253FF',
+    padding: 10,
+    borderRadius: 10,
+    width: width / 2.2,
+    height: 80
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  subcontainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: width,
+    paddingHorizontal: 24,
+    height: 400,
+    marginTop: 16
+  },
+  image: {
+    width: 250,
+    height: 250,
+    alignSelf: 'center'
+  }
+})
