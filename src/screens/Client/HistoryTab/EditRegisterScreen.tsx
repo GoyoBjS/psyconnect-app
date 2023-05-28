f1f62cc7013faf2077bb95fcaa462c536fa5d1ee
Gregorio Bujor Santos <goyobujor@gmail.com>
2023-05-28 21:54:19 +0200

    feat: add reasons to modify

--- src/screens/Client/HistoryTab/EditRegisterScreen.tsx
+++ src/screens/Client/HistoryTab/EditRegisterScreen.tsx
@@ -5,9 +5,10 @@ import { reasons } from '../../../components/reasons'
 import FeelingsScreen from '../HomeTab/Registrations/Steps/FeelingsScreen'
 import ReasonScreen from '../HomeTab/Registrations/Steps/ReasonScreen'
 import ElementToModify from './components/ElementToModify'
+import { solutions } from '../../../components/solutions'
+import SolutionScreen from '../HomeTab/Registrations/Steps/SolutionScreen'
 
 const EditRegisterScreen = ({
-  navigation,
   route: {
     params: { item }
   }
@@ -24,33 +25,54 @@ const EditRegisterScreen = ({
         <FeelingsScreen setData={setData} handleClose={handleClose} />
       )}
       {showEditScreen === 'reason' && <ReasonScreen setData={setData} handleClose={handleClose} />}
-      <ScrollView>
-        <Text style={{ fontSize: 24, fontWeight: 'bold', margin: 12 }}>Editar registro</Text>
-        <ElementToModify
-          data={data}
-          setData={setData}
-          title="¿Cómo te sientes?"
-          element="feeling"
-          handlePress={handlePress}
-          styleData={feelings}
-        />
-        <ElementToModify
-          data={data}
-          setData={setData}
-          title="¿Qué lo ha causado?"
-          element="reason"
-          handlePress={handlePress}
-          styleData={reasons}
-        />
+      {showEditScreen === 'solution' && (
+        <SolutionScreen setData={setData} handleClose={handleClose} />
+      )}
+      {!showEditScreen && (
+        <ScrollView>
+          <Text style={{ fontSize: 24, fontWeight: 'bold', margin: 12 }}>Editar registro</Text>
+          <ElementToModify
+            data={data}
+            setData={setData}
+            title="¿Cómo te sientes?"
+            element="feeling"
+            handlePress={handlePress}
+            styleData={feelings}
+          />
+          <ElementToModify
+            data={data}
+            setData={setData}
+            title="¿Qué lo ha causado?"
+            element="reason"
+            handlePress={handlePress}
+            styleData={reasons}
+          />
+          {/*<ElementToModify*/}
+          {/*  data={data}*/}
+          {/*  setData={setData}*/}
+          {/*  title="¿Cuándo ha sido?"*/}
+          {/*  element="dateTime"*/}
+          {/*  handlePress={handlePress}*/}
+          {/*  styleData={reasons}*/}
+          {/*/>*/}
+          <ElementToModify
+            data={data}
+            setData={setData}
+            title="¿Se ha solucionado?"
+            element="solution"
+            handlePress={handlePress}
+            styleData={solutions}
+          />
 
-        {/* <Text>{id}</Text>
+          {/* <Text>{id}</Text>
             <Text>{feeling}</Text>
             <Text>{reason}</Text>
             <Text>{date}</Text>
             <Text>{time}</Text>
             <Text>{timestamp}</Text>
             <Text>{solution}</Text> */}
-      </ScrollView>
+        </ScrollView>
+      )}
     </>
   )
 }
